e1d329ab0938f98835f5d1378b45041742194389
marcos.remon <marcos.remon@kintech-engineering.com>
2025-07-01 12:39:37 +0200

    friend_controller y friend_repository reformateados mejores practicas (unica salida, usando elses)

--- service/src/infraestructure/persistence/friend_repository.py
+++ service/src/infraestructure/persistence/friend_repository.py
@@ -3,6 +3,7 @@ from select import select
 from src.application.mappers.user_mapper import UserMapper
 from src.core.interfaces.repository.abstract_friend_repository import AbstractFriendRepository
 from src.core.model.entities import User, UserFriend
+from src.transversal.common.response_codes_json import ResponseCodesJson
 from src.transversal.request_response.friend.add_new_user_friend.add_new_user_friend_request import \
     AddNewUserFriendRequest
 from src.transversal.request_response.friend.add_new_user_friend.add_new_user_friend_response import \
@@ -23,116 +24,121 @@ class FriendRepository(AbstractFriendRepository):
         try:
             user = await self._session.query(User).filter_by(email = get_all_user_friends_request.email).first()
             if user is None:
-                return GetAllUserFriendsResponse(
+                get_all_user_friends_response = GetAllUserFriendsResponse(
                     is_success = False,
-                    message = "user not found",
-                    response_codes_json = 404
+                    message = str(f"user not found"),
+                    response_codes_json = ResponseCodesJson.NOT_FOUND,
                 )
-
-            user_friends = await self._session.query(UserFriend).filter_by(user_id = user.id).all()
-            if len(user_friends) == 0:
-                return GetAllUserFriendsResponse(
-                    is_success = False,
-                    message = "user not have any friends",
-                    response_codes_json = 404
-                )
-
-            return GetAllUserFriendsResponse(
-                is_success = True,
-                message = "user has friends",
-                response_codes_json = 200,
-                friends = UserMapper.map_user_list(user_friends)
-            )
+            else:
+                user_friends = await self._session.query(UserFriend).filter_by(user_id = user.id).all()
+                if len(user_friends) == 0:
+                    get_all_user_friends_response = GetAllUserFriendsResponse(
+                        is_success = False,
+                        message = str(f"user not have any friends"),
+                        response_codes_json = ResponseCodesJson.NOT_FOUND
+                    )
+                else:
+                    get_all_user_friends_response = GetAllUserFriendsResponse(
+                        is_success = True,
+                        message = str(f"user has friends"),
+                        response_codes_json = ResponseCodesJson.OK,
+                        friends = UserMapper.map_user_list(user_friends)
+                    )
         except Exception as e:
-            return GetAllUserFriendsResponse(
+            get_all_user_friends_response = GetAllUserFriendsResponse(
                 is_success = False,
-                message = f"unexpected error in get_all_user_friends repository: {e}",
-                response_codes_json = 500
+                message = str(f"unexpected error in get_all_user_friends repository: {e}"),
+                response_codes_json = ResponseCodesJson.INTERNAL_SERVER_ERROR
             )
+
+        return get_all_user_friends_response
 
     async def add_new_user_friend(self, add_new_user_friend_request: AddNewUserFriendRequest) -> AddNewUserFriendResponse:
         try:
             user = await self._session.query(User).filter_by(email = add_new_user_friend_request.email).first()
             if user is None:
-                return AddNewUserFriendResponse(
+                add_new_user_friend_response = AddNewUserFriendResponse(
                     is_success = False,
-                    message = "user not found",
-                    response_codes_json = 404
+                    message = str(f"user not found"),
+                    response_codes_json = ResponseCodesJson.NOT_FOUND
                 )
+            else:
+                friend = await self._session.query(User).filter_by(friend_code = add_new_user_friend_request.friend_code).first()
+                if friend is None:
+                    add_new_user_friend_response = AddNewUserFriendResponse(
+                        is_success = False,
+                        message = str(f"user not have any friends"),
+                        response_codes_json = ResponseCodesJson.NOT_FOUND
+                    )
+                else:
+                    user_friend = UserFriend(
+                        user_id = user.id,
+                        friend_id = friend.id,
+                    )
 
-            friend = await self._session.query(User).filter_by(friend_code = add_new_user_friend_request.friend_code).first()
-            if friend is None:
-                return AddNewUserFriendResponse(
-                    is_success = False,
-                    message = "user not have any friends",
-                    response_codes_json = 404
-                )
+                    await self._session.add(user_friend)
+                    await self._session.commit()
 
-            user_friend = UserFriend(
-                user_id = user.id,
-                friend_id = friend.id,
-            )
-
-            await self._session.add(user_friend)
-            await self._session.commit()
-
-            return AddNewUserFriendResponse(
-                is_success = True,
-                message = "user added friend",
-                response_codes_json = 200,
-                friend_dto = UserMapper.map_user(friend)
-            )
+                    add_new_user_friend_response = AddNewUserFriendResponse(
+                        is_success = True,
+                        message = str(f"user added friend"),
+                        response_codes_json = ResponseCodesJson.OK,
+                        friend_dto = UserMapper.map_user(friend)
+                    )
         except Exception as e:
-            return AddNewUserFriendResponse(
+            add_new_user_friend_response = AddNewUserFriendResponse(
                 is_success = False,
-                message = f"unexpected error in add_new_user_friend repository {e}",
-                response_codes_json = 500
+                message = str(f"unexpected error in add_new_user_friend repository {e}"),
+                response_codes_json = ResponseCodesJson.INTERNAL_SERVER_ERROR
             )
+
+        return add_new_user_friend_response
 
     async def delete_friend(self, friend_request: DeleteFriendRequest) -> DeleteFriendResponse:
         try:
             user = await self._session.query(User).filter_by(email = friend_request.email).first()
             if user is None:
-                return DeleteFriendResponse(
+                delete_friend_response = DeleteFriendResponse(
                     is_success = False,
-                    message = "user not found",
-                    response_codes_json = 404
+                    message = str(f"user not found"),
+                    response_codes_json = ResponseCodesJson.NOT_FOUND
                 )
+            else:
+                friend = await self._session.query(User).filter_by(email = friend_request.friend_email).first()
+                if friend is None:
+                    delete_friend_response = DeleteFriendResponse(
+                        is_success = False,
+                        message = str(f"friend not found"),
+                        response_codes_json = ResponseCodesJson.NOT_FOUND
+                    )
+                else:
+                    user_friend = (await self._session.execute(
+                        select(UserFriend).where(
+                            (UserFriend.user_id == friend.user_id) &
+                            (UserFriend.friend_id == friend.friend_id)
+                        )
+                    )).scalars().first()
+                    if user_friend is None:
+                        delete_friend_response = DeleteFriendResponse(
+                            is_success = False,
+                            message = str(f"they are not friends"),
+                            response_codes_json = ResponseCodesJson.NOT_FOUND
+                        )
+                    else:
+                        await self._session.delete(user_friend)
+                        await self._session.commit()
 
-            friend = await self._session.query(User).filter_by(email = friend_request.friend_email).first()
-            if friend is None:
-                return DeleteFriendResponse(
-                    is_success = False,
-                    message = "friend not found",
-                    response_codes_json = 404
-                )
-
-            user_friend = await self._session.scalars(
-                select(UserFriend)
-                .Where(
-                    (UserFriend.user_id == friend.user_id) &
-                    (UserFriend.friend_id == friend.friend_id)
-                ).first()
-            )
-            if user_friend is None:
-                return DeleteFriendResponse(
-                    is_success = False,
-                    message = "they are not friends",
-                    response_codes_json = 404
-                )
-
-            await self._session.delete(user_friend)
-            await self._session.commit()
-
-            return DeleteFriendResponse(
-                is_success = True,
-                message = "friend deleted",
-                response_codes_json = 200,
-                user_dto = UserMapper.map_user(user)
-            )
+                        delete_friend_response = DeleteFriendResponse(
+                            is_success = True,
+                            message = str(f"friend deleted"),
+                            response_codes_json = ResponseCodesJson.OK,
+                            user_dto = UserMapper.map_user(user)
+                        )
         except Exception as e:
-            return DeleteFriendResponse(
+            delete_friend_response = DeleteFriendResponse(
                 is_success = False,
-                message = f"unexpected error in delete_friend repository {e}",
-                response_codes_json = 500
+                message = str(f"unexpected error in delete_friend repository {e}"),
+                response_codes_json = ResponseCodesJson.INTERNAL_SERVER_ERROR
             )
+
+        return delete_friend_response
--- service/src/service/controller/friend_controller.py
+++ service/src/service/controller/friend_controller.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from starlette.responses import JSONResponse
 
 from src.infraestructure.dependency.dependency_injection import get_user_application
+from src.transversal.common.response_codes_json import ResponseCodesJson
 from src.transversal.request_response.friend.add_new_user_friend.add_new_user_friend_request import \
     AddNewUserFriendRequest
 from src.transversal.request_response.friend.add_new_user_friend.add_new_user_friend_response import \
@@ -18,77 +19,134 @@ http = APIRouter(prefix="/api/friend")
 @http.post("/get-all-user-friends", response_model = GetAllUserFriendsResponse)
 async def get_all_user_friends(get_all_user_friends_request: GetAllUserFriendsRequest, application = Depends(get_user_application)):
     try:
-        get_all_user_friends_response = await application.get_all_user_friends(get_all_user_friends_request)
-        if get_all_user_friends_response.is_success:
-            return JSONResponse(
-                content = get_all_user_friends_response.to_dict(),
-                status_code = get_all_user_friends_response.response_codes_json
+        if get_all_user_friends_request is None or get_all_user_friends_request.email is None:
+            json_response = JSONResponse(
+                status_code = ResponseCodesJson.BAD_REQUEST,
+                content = GetAllUserFriendsResponse(
+                    response_codes_json = ResponseCodesJson.BAD_REQUEST,
+                    is_success = False,
+                    message = str(f"invalid data, email is required")
+                )
             )
-
-        return JSONResponse(
-            content = get_all_user_friends_response.to_dict(),
-            status_code = get_all_user_friends_response.response_codes_json
-        )
+        else:
+            get_all_user_friends_response = await application.get_all_user_friends(get_all_user_friends_request)
+            if get_all_user_friends_response.is_success:
+                json_response = JSONResponse(
+                    status_code = get_all_user_friends_response.response_codes_json,
+                    content = GetAllUserFriendsResponse(
+                        response_codes_json = get_all_user_friends_response.response_codes_json,
+                        is_success = get_all_user_friends_response.is_success,
+                        message = get_all_user_friends_response.message,
+                        friends = get_all_user_friends_response.friends,
+                    )
+                )
+            else:
+                json_response = JSONResponse(
+                    status_code = get_all_user_friends_response.response_codes_json,
+                    content = GetAllUserFriendsResponse(
+                        response_codes_json = get_all_user_friends_response.response_codes_json,
+                        is_success = get_all_user_friends_response.is_success,
+                        message = get_all_user_friends_response.message,
+                    )
+                )
     except Exception as e:
-        error_response = GetAllUserFriendsResponse(
-            is_success = False,
-            message = f"unexpected error in get-all-user-friends in controller: {e}",
-            response_codes_json = 500
+        json_response = JSONResponse(
+            status_code = ResponseCodesJson.INTERNAL_SERVER_ERROR,
+            content = GetAllUserFriendsResponse(
+                response_codes_json = ResponseCodesJson.INTERNAL_SERVER_ERROR,
+                is_success = False,
+                message = str(f"unexpected error on get-all-user-friend controller: {e}")
+            )
         )
 
-        return JSONResponse(
-            content = error_response.to_dict(),
-            status_code = error_response.response_codes_json
-        )
+    return json_response
 
 @http.post("/add-new-user-friend", response_model = AddNewUserFriendResponse)
 async def add_new_user_friend(add_new_user_friend_request: AddNewUserFriendRequest, application = Depends(get_user_application)):
     try:
-        add_new_user_friend_response = await application.get_all_user_friends(add_new_user_friend_request)
-        if add_new_user_friend_response.is_success:
-            return JSONResponse(
-                content = add_new_user_friend_response.to_dict(),
-                status_code = add_new_user_friend_response.response_codes_json
+        if add_new_user_friend_request is None or add_new_user_friend_request.email is None or add_new_user_friend_request.friend_code is None:
+            json_response = JSONResponse(
+                status_code = ResponseCodesJson.BAD_REQUEST,
+                content = AddNewUserFriendResponse(
+                    response_codes_json = ResponseCodesJson.BAD_REQUEST,
+                    is_success = False,
+                    message = str(f"invalid data, email or friend code are required")
+                )
             )
-
-        return JSONResponse(
-            content = add_new_user_friend_response.to_dict(),
-            status_code = add_new_user_friend_response.response_codes_json
-        )
+        else:
+            add_new_user_friend_response = await application.get_all_user_friends(add_new_user_friend_request)
+            if add_new_user_friend_response.is_success:
+                json_response = JSONResponse(
+                    status_code = add_new_user_friend_response.response_codes_json,
+                    content = AddNewUserFriendResponse(
+                        response_codes_json = add_new_user_friend_response.response_codes_json,
+                        is_success = add_new_user_friend_response.is_success,
+                        message = add_new_user_friend_response.message,
+                        friend_dto = add_new_user_friend_response.friend_dto,
+                    )
+                )
+            else:
+                json_response = JSONResponse(
+                    status_code = add_new_user_friend_response.response_codes_json,
+                    content = AddNewUserFriendResponse(
+                        response_codes_json = add_new_user_friend_response.response_codes_json,
+                        is_success = add_new_user_friend_response.is_success,
+                        message = add_new_user_friend_response.message,
+                    )
+                )
     except Exception as e:
-        error_response = AddNewUserFriendResponse(
-            is_success = False,
-            message = f"unexpected error in add-new-user-friend in controller: {e}",
-            response_codes_json = 500
+        json_response = JSONResponse(
+            status_code = ResponseCodesJson.INTERNAL_SERVER_ERROR,
+            content = AddNewUserFriendResponse(
+                response_codes_json = ResponseCodesJson.INTERNAL_SERVER_ERROR,
+                is_success = False,
+                message = str(f"unexpected error on add-new-user-friend controller: {e}")
+            )
         )
 
-        return JSONResponse(
-            content = error_response.to_dict(),
-            status_code = error_response.response_codes_json
-        )
+    return json_response
 
 @http.post("/delete-friend", response_model = DeleteFriendResponse)
 async def get_all_user_friends(delete_friend_request: DeleteFriendRequest, application = Depends(get_user_application)):
     try:
-        delete_friend_response = await application.get_all_user_friends(delete_friend_request)
-        if delete_friend_response.is_success:
-            return JSONResponse(
-                content = delete_friend_response.to_dict(),
-                status_code = delete_friend_response.response_codes_json
+        if delete_friend_request is None or delete_friend_request.email is None or delete_friend_request.friend_email is None:
+            json_response = JSONResponse(
+                status_code = ResponseCodesJson.BAD_REQUEST,
+                content = DeleteFriendResponse(
+                    response_codes_json = ResponseCodesJson.BAD_REQUEST,
+                    is_success = False,
+                    message = str(f"invalid data, email or friend code are required")
+                )
             )
-
-        return JSONResponse(
-            content = delete_friend_response.to_dict(),
-            status_code = delete_friend_response.response_codes_json
-        )
+        else:
+            delete_friend_response = await application.get_all_user_friends(delete_friend_request)
+            if delete_friend_response.is_success:
+                json_response = JSONResponse(
+                    status_code = delete_friend_response.response_codes_json,
+                    content = DeleteFriendResponse(
+                        response_codes_json = delete_friend_response.response_codes_json,
+                        is_success = delete_friend_response.is_success,
+                        message = delete_friend_response.message,
+                        user_dto = delete_friend_response.user_dto,
+                    )
+                )
+            else:
+                json_response = JSONResponse(
+                    status_code = delete_friend_response.response_codes_json,
+                    content = DeleteFriendResponse(
+                        response_codes_json = delete_friend_response.response_codes_json,
+                        is_success = delete_friend_response.is_success,
+                        message = delete_friend_response.message,
+                    )
+                )
     except Exception as e:
-        error_response = DeleteFriendResponse(
-            is_success = False,
-            message = f"unexpected error in delete-friend in controller: {e}",
-            response_codes_json = 500
+        json_response = JSONResponse(
+            status_code = ResponseCodesJson.INTERNAL_SERVER_ERROR,
+            content = DeleteFriendResponse(
+                response_codes_json = ResponseCodesJson.INTERNAL_SERVER_ERROR,
+                is_success = False,
+                message = str(f"unexpected error on delete-friend: {e}")
+            )
         )
 
-        return JSONResponse(
-            content = error_response.to_dict(),
-            status_code = error_response.response_codes_json
-        )
+    return json_response
